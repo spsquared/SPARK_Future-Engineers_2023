0f74713f391a70edf7d432909dc3f5b9ee1f38bc
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2023-07-08 11:11:59 -0400

    probably broken server implementation

--- Program/Util/server.js
+++ Program/Util/server.js
@@ -1,23 +1,38 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const cors = require('cors');
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}));
+const server2 = require('http').Server(app);
+const { Server: SocketIO } = require('socket.io');
 
 app.get('/', (req, res) => res.send('OK'));
 
-const io = require('socket.io')(server, {
+const hostio = new SocketIO(server);
+const io = new SocketIO(server2, {
     cors: {
         origin: '*',
         methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
     }
 });
-
 const authIds = require('./auth.json');
 
+const recentData = {};
+let hostConnectionCount = 0;
+hostio.on('connection', (socket) => {
+    hostConnectionCount++;
+    socket.on('disconnect', () => hostConnectionCount--);
+    socket.on('timeout', () => hostConnectionCount--);
+    socket.on('error', () => hostConnectionCount--);
+    if (hostConnectionCount > 1) {
+        io.emit('multipleHosts');
+        hostio.emit('multipleHosts');
+    }
+    socket.onAny((event, ...args) => { // python socketio only allows 1 argument but sure
+        io.emit(event, ...args);
+        if (event === 'data') {
+            // store in recentData
+        }
+    });
+});
 io.on('connection', (socket) => {
     socket.emit('authenticate');
     socket.once('authenticateResponse', (id) => {
@@ -25,8 +40,25 @@ io.on('connection', (socket) => {
             socket.disconnect();
             return;
         }
-        socket.emit('test');
+        socket.on('error', (err) => console.log(err));
+        const onevent = socket.onevent;
+        socket.onevent = (packet) => {
+            if (packet.data[0] == null) {
+                socket.disconnect();
+                return;
+            }
+            onevent.call(socket, packet);
+        };
+        socket.onAny((event, ...args) => {
+            if (event === 'error') return;
+            if (event === 'getRecentData') {
+                socket.emit('recentData', recentData);
+                return;
+            }
+            hostio.emit(event, args); // arguments are condensed into one array for python socketio
+        });
     });
 });
 
-server.listen(4040);
+server.listen(4041);
+server2.listen(4040);
--- Program/Util/server.py
+++ Program/Util/server.py
@@ -1,1 +1,21 @@
-# PYTHON SOCKET.IO ????
+import socketio
+import asyncio
+import typing
+
+# connects to internal proxy and uses that as server
+
+__socket = socketio.AsyncClient()
+
+async def __start():
+    await __socket.connect('http://localhost:4041')
+asyncio.run(__start())
+
+def on(ev: str, cb: typing.Callable[[typing.Any], None]):
+    @__socket.on(ev)
+    def handle(data):
+        cb(data)
+
+async def __emit(ev, data):
+    await __socket.emit(ev, data)
+def emit(ev: str, data):
+    asyncio.create_task(__emit(ev, data))
