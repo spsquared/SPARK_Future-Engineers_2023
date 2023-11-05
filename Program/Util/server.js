ffdc24ee826003597284e07446c2117ecb753bc0
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2023-11-05 09:40:05 -0500

    fix manual drive not stopping?

--- Program/Util/server.js
+++ Program/Util/server.js
@@ -105,7 +105,7 @@ function runProgram(mode) {
         }
     }
     io.emit('#programStarting', mode);
-    const program = subprocess.spawn('python3', [path.resolve(mode == 'manual' ? 'manualdrive.py' : 'autodrive.py')]);
+    const program = subprocess.spawn('python3', [path.resolve(mode == 'manual' ? 'manualdrive.py no_terminal' : 'autodrive.py')]);
     program.stdout.setEncoding('utf8');    
     program.stderr.setEncoding('utf8');
     program.stdout.on('data', (chunk) => {
